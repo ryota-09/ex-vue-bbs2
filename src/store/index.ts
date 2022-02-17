f73d57b5880a2c8948918e1a42741be380734fb7
ryota-09 <n.ryo950911@gmail.com>
2022-02-17 14:33:47 +0900

    (1)記事一覧表示完了

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { Article } from "@/types/article";
+// import { Comment } from "@/types/comment";
+import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   state: {
-  },
-  mutations: {
+    articles: new Array<Article>(),
   },
   actions: {
+    async addArticle(context){
+      const res = await axios.get("http://153.127.48.168:8080/ex-bbs-api/bbs/articles");
+
+      console.dir(JSON.stringify(res));
+      const payload = res.data;
+      context.commit("addArticles",payload);
+    }
+  },
+  mutations: {
+    addArticles(state, payload){
+      state.articles = new Array<Article>();
+      for(const article of payload.articles){
+        state.articles.push(
+          new Article(
+            article.id,
+            article.name,
+            article.content,
+            article.commentList
+          )
+        );
+      }
+    }
+  },
+  getters: {
+    getArticles(state){
+      return state.articles;
+    }
   },
   modules: {
-  }
+  },
 })
---
+++ src/views/Bbs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bbs">
+    <h1>掲示板アプリ</h1>
+    <div class="post-area">
+      <div></div>
+    </div>
+    <hr>
+    <div class="time-line-area">
+      <div class="article-container" v-for="article of currentArticleList" v-bind:key="article.id">
+        <div class="article-name">{{ article.name }}</div>
+        <div class="article-content">{{ article.content }}</div>
+        <div class="article-content">{{ article.commentList }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Article } from "@/types/article";
+// import { Comment } from "@/types/comment";
+@Component
+export default class Bbs extends Vue {
+  private currentArticleList = new Array<Article>();
+
+  async created(): Promise<void>{
+    await this.$store.dispatch("addArticle");
+    this.currentArticleList = this.$store.getters.getArticles;
+  }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
